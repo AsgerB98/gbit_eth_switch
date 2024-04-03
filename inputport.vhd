e3bb1edd2e21d5f99d9bd75561b25215655dbb8d
OliverAJeppesen <oliver_j@ymail.com>
2024-04-03 16:37:30 +0200

    Inputport is created. FCS must be changed. Need to work on SoF.

---
+++ inputport.vhd
@@ -0,0 +1,85 @@
+library ieee;
+use std.textio.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+
+entity inputport is
+    port (
+        clk     : in std_logic;
+        reset   : in std_logic;
+        data_in : in std_logic_vector (7 downto 0);
+        valid   : in std_logic;
+        read_en : in std_logic;
+
+        srcMac : out std_logic_vector(47 downto 0);
+        dstMac : out std_logic_vector(47 downto 0);
+        FCS_error : out std_logic;
+        data_out: out std_logic_vector (7 downto 0)
+    );
+end entity;
+
+
+architecture inputport_arch of inputport is
+   
+
+  signal empty_fifo : std_logic := '0';
+  signal full_fifo : std_logic := '0';
+  signal status_fifo : std_logic_vector (10 downto 0) := (others => '0');
+
+  signal SoF  : std_logic := '0';
+  signal EoF  : std_logic := '0'; 
+  
+  
+
+  component FIFOSwitch is
+    port (
+      clock		: IN STD_LOGIC ;
+      data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+      rdreq		: IN STD_LOGIC ;
+      wrreq		: IN STD_LOGIC ;
+      empty		: OUT STD_LOGIC ;
+      full		: OUT STD_LOGIC ;
+      q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+      usedw		: OUT STD_LOGIC_VECTOR (10 DOWNTO 0)
+    );
+  end component;
+
+  component FCS is
+    port (
+      clk : in std_logic;
+      reset : in std_logic;
+      start_of_frame : in std_logic;
+      end_of_frame : in std_logic;
+      data_in : in std_logic_vector(7 downto 0);
+      fcs_error : out std_logic
+    );
+  end component;
+begin
+
+-- Port mapping
+  fifo_ports : FIFOSwitch
+    port map (
+      clock => clk,
+      data => data_in,
+      rdreq => read_en,
+      wrreq => valid,
+      empty => empty_fifo,
+      full => full_fifo,
+      q => data_out,
+      usedw => status_fifo
+    );
+
+fcs_ports : FCS
+port map (
+    clk  =>clk,
+    reset  =>reset,
+    start_of_frame  => SoF, 
+    end_of_frame  => EoF, 
+    data_in  => data_in,
+    fcs_error => FCS_error
+    );
+    
+
+end architecture;
---
+++ inputport_tb.vhd
@@ -0,0 +1,76 @@
+library IEEE;
+library std;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.numeric_std_unsigned.all;
+use std.textio.all;
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
+
+entity inputport_tb is
+end;
+
+architecture bench of inputport_tb is
+  constant clk_period : time := 5 ns;
+
+  signal clk : std_logic;
+  signal reset : std_logic;
+  signal data_in : std_logic_vector (7 downto 0);
+  signal valid : std_logic;
+  signal data_out : std_logic_vector (7 downto 0);
+
+  component inputport is
+    port(
+      clk     : in std_logic;
+      reset   : in std_logic;        
+      data_in : in std_logic_vector (7 downto 0);
+      valid   : in std_logic;
+  
+      data_out : out std_logic_vector (7 downto 0)
+    );
+  end component;
+begin
+
+  dut : inputport
+  port map (
+    clk => clk,
+    reset => reset,
+    data_in => data_in,
+    valid => valid,
+    data_out => data_out
+  );
+-- clk <= not clk after clk_period/2;
+
+
+
+
+
+
+clk_process : process
+begin
+  clk <= '1'; wait for clk_period/2;
+  clk <= '0'; wait for clk_period/2;
+end process clk_process;
+
+reading_proc : process (clk)
+  file input : TEXT open READ_MODE is "Input_packet.txt"; 
+    
+  variable current_read_line	: line;
+  variable current_read_field	: std_logic_vector (7 downto 0);
+  variable current_write_line : std_logic_vector (0 downto 0);
+  variable start_of_data_Reader : std_logic;
+
+begin
+
+  if rising_edge(clk) then
+    readline(input, current_read_line);
+    hread(current_read_line, current_read_field);
+
+    data_in <= current_read_field;
+end if;
+end process;
+
+
+
+
+end;
